3a71c2d6852f51356d39e273c2a5286f59a6f132
Alex Elkin <elckinne@gmail.com>
2021-01-19 18:53:57 +0300

    chore(project): Cofigure simple chat example.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.info("hello")
+import {Telegraf} from 'telegraf'
+
+const bot = new Telegraf("1577224234:AAG6mx9R6FKjAQWl9ZH2rGH5KhQYB4MCtTw")
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hello !!!!!!!'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
