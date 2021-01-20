9c43f44f269f83e5861cde00501e2e8de7ce3544
Alex Elkin <elckinne@gmail.com>
2021-01-20 18:29:15 +0300

    feat(spreadsheet): Add Google Spreadsheet support.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,55 @@
-import {Telegraf} from 'telegraf'
+import { assert } from 'console'
+import { Telegraf } from 'telegraf'
+import { GoogleSpreadsheet } from 'google-spreadsheet'
 
-const bot = new Telegraf("1577224234:AAG6mx9R6FKjAQWl9ZH2rGH5KhQYB4MCtTw")
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hello !!!!!!!'))
-bot.launch()
+const tgToken = process.env["TG_BOT_TOKEN"]
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+assert(tgToken != null, "No TG_BOT_TOKEN environment variable found")
+
+const bot = new Telegraf(tgToken!)
+
+const doc = new GoogleSpreadsheet("1qPm39FBUKRPVrk2jl3e8smeSb-yggdTo9IlCx7ff1Hc")
+
+const moneyMgrEmail = process.env["MONEY_MGR_EMAIL"]
+const moneyMgrKey = process.env["MONEY_MGR_KEY"]
+assert(moneyMgrEmail != null, "No MONEY_MGR_EMAIL environment variable found")
+assert(moneyMgrKey != null, "No MONEY_MGR_KEY environment variable found")
+
+start()
+
+async function start() {
+    await doc.useServiceAccountAuth({
+        client_email: moneyMgrEmail!,
+        private_key: moneyMgrKey!.replace(/\\n/g, '\n')
+    })
+
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[0]
+
+    bot.start((ctx) => ctx.reply('Welcome'))
+
+    bot.on('text', async (ctx) => {
+
+        console.info(ctx.message.text)
+
+        const r = /(?<date>\w+)\s+(?<type>\w+)\s+(?<category>\w+)\s+(?<amount>\d+)\s+(?<description>\w+)/
+            .exec(ctx.message.text)
+
+        if (r == null) ctx.reply("Неправильный текст")
+        else
+            await sheet.addRow({
+                date: r?.groups?.date!,
+                type: r?.groups?.type!,
+                category: r?.groups?.category!,
+                amount: r?.groups?.amount!,
+                description: r?.groups?.description!
+            })
+    })
+
+    bot.launch()
+
+    // Enable graceful stop
+    process.once('SIGINT', () => bot.stop('SIGINT'))
+    process.once('SIGTERM', () => bot.stop('SIGTERM'))
+}
+
