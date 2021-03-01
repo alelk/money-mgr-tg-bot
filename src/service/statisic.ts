57c3f51df888f615b970dd6435a2f8ab8516de46
Alex Elkin <alelkdev@gmail.com>
2021-03-01 19:38:54 +0300

    fix(statistic): Update month statistic.

--- src/service/statisic.ts
+++ src/service/statisic.ts
@@ -1,6 +1,6 @@
 import { AbstractGoogleSpreadsheetService } from "."
 import { CategoryStatistic, MonthStatustic, TransactionTypeStatistic } from "../model/statistic"
-import { Category, TransactionType } from "../model"
+import { Category, CategoryRelationToParent, TransactionDirection, TransactionType } from "../model"
 import { categoriesSvc, transactionTypesSvc } from "../services"
 
 type RawCategoryStatistic = { category: Category, amount: number }
@@ -8,7 +8,7 @@ type RawCategoryStatistic = { category: Category, amount: number }
 export class StatisticService extends AbstractGoogleSpreadsheetService {
 
     categoriesPromise: Promise<Category[]> = categoriesSvc.getCategories()
-    transactionTypesPrimise: Promise<TransactionType[]> = transactionTypesSvc.getTransactionTypes()
+    transactionTypesPromise: Promise<TransactionType[]> = transactionTypesSvc.getTransactionTypes()
 
     constructor(sheetId: string, authEmail: string, authKey: string) {
         super(sheetId, authEmail, authKey)
@@ -19,22 +19,31 @@ export class StatisticService extends AbstractGoogleSpreadsheetService {
         const childStatistic =
             (await Promise.all(childCategories.map(c => this.categoryStatistic(c, rawCategoryStatistic))))
                 .filter(v => v != null) as CategoryStatistic[]
-        const childAmount = childStatistic.reduce((acc, c) => acc + c.amount, 0)
-        const amount = rawCategoryStatistic.find(s => s.category == category)?.amount
-        if (amount == null && childStatistic.length === 0) return undefined
-        else return new CategoryStatistic(category, (amount == null ? 0 : amount) + childAmount, childStatistic)
+        const childAmount = childStatistic.reduce((acc, c) => c.category.relationToParent == CategoryRelationToParent.POSITIVE ? acc + c.amount : acc - c.amount, 0)
+        const amount = rawCategoryStatistic.find(s => s.category.name == category.name)?.amount
+        if ((amount == null || amount == 0) && childStatistic.length === 0) return undefined
+        else return new CategoryStatistic(category, (amount == null ? 0 : category.relationToParent === CategoryRelationToParent.POSITIVE ? amount : -amount) + childAmount, childStatistic)
     }
 
     async getStatistic(): Promise<MonthStatustic[]> {
         const d = await this.doc
+        d.resetLocalCache()
+        await d.loadInfo()
         const sheet = d.sheetsByTitle["month report by category"]
         const rows = (await sheet.getRows()).filter(r => /20\d\d-\w\w\w/.test(r["date - Year-Month"]))
         const categories = await this.categoriesPromise
-        const transactionTypes = await this.transactionTypesPrimise
-        const statisticCategories = sheet.headerValues.map(h => categories.find(c => c.name === h)).filter(c => c != null) as Category[]
+        const transactionTypes = await this.transactionTypesPromise
+        const statisticCategories =
+            sheet.headerValues.filter(h => h != 'date - Year-Month').map(h => {
+                const c = categories.find(c => c.name === h)
+                if (c == null) throw new Error(`Category not found: ${h}`)
+                return c
+            }) as Category[]
         const monthStatistic = rows.map(r => ({
             date: new Date(Date.parse(r["date - Year-Month"])),
-            categories: statisticCategories.reduce((acc, c) => [...acc, { category: c, amount: Number.parseFloat(r[c.name]) }], [] as RawCategoryStatistic[])
+            categories: statisticCategories.reduce((acc, c) =>
+                [...acc, { category: c, amount: Number.parseFloat(r[c.name] == null || r[c.name].trim() === '' ? 0 : r[c.name]) }]
+                , [] as RawCategoryStatistic[])
         }))
         const rootCategories = categories.filter(c => c.parentCategoryName == null)
         return await Promise.all(
@@ -51,7 +60,20 @@ export class StatisticService extends AbstractGoogleSpreadsheetService {
                         return new TransactionTypeStatistic(t, amount, transactionTypeCategoriesStatistic)
                     }))
                 ).filter(v => v != null) as TransactionTypeStatistic[]
-                return new MonthStatustic(s.date, transactionTypeStatistic)
+                const total = transactionTypeStatistic.reduce(({ formula, amount }, s) =>
+                    s.transactionType.direction === TransactionDirection.INCOME //&& amount !== 0
+                        ? {
+                            formula: (formula === '' ? `${s.amount} [${s.transactionType.name}]` : `${formula} + ${s.amount} [${s.transactionType.name}]`),
+                            amount: amount + s.amount
+                        }
+                        : s.transactionType.direction === TransactionDirection.OUTCOME //&& amount !== 0
+                            ? {
+                                formula: (formula === '' ? `${s.amount} [${s.transactionType.name}]` : `${formula} - ${s.amount} [${s.transactionType.name}]`),
+                                amount: amount - s.amount
+                            }
+                            : { formula, amount },
+                    { formula: '', amount: 0 })
+                return new MonthStatustic(s.date, transactionTypeStatistic, `${total.formula} = ${total.amount} руб.`)
             })
         )
     }
