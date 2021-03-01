c9db9b68cfb895b6eaeb1445939487102f9811b5
Alex Elkin <elckinne@gmail.com>
2021-03-01 19:11:36 +0300

    refactor(parser): Statistic request parser refactoring.

--- src/parser/statisticRequest.ts
+++ src/parser/statisticRequest.ts
@@ -9,14 +9,18 @@ interface StatisticRequestSpec {
 
 export const statisticRequestParser = P.createLanguage<StatisticRequestSpec>({
     date: () => P.alt(
-        P.regexp(/прошлый\s+месяц/i).map(() => { const d = new Date(); d.setMonth(d.getMonth() - 1); return d }),
-        P.regexp(/позапрошлый\s+месяц/i).map(() => { const d = new Date(); d.setMonth(d.getMonth() - 2); return d }),
-        P.regexp(/позапрошлый\s+месяц/i).map(() => { const d = new Date(); d.setMonth(d.getMonth() - 2); return d }),
+        P.regexp(/(этот|текущий|последний\s+)?месяц/i).map(() => new Date()),
+        P.regexp(/прошлый\s+месяц/i).map(() => shiftCurrentMonth(-1)),
+        P.regexp(/позапрошлый\s+месяц/i).map(() => shiftCurrentMonth(-2)),
         P.digits
             .skip(P.optWhitespace.then(P.regexp(/месяц(ев)?\s+назад/i)))
-            .desc("сколько месяцев назад, например '2 месяца назад'").map((n) => {
-                const d = new Date(); d.setMonth(d.getMonth() - Number.parseInt(n)); return d
-            })
+            .desc("сколько месяцев назад, например '2 месяца назад'").map((n) => shiftCurrentMonth(Number.parseInt(n)))
     ),
     statisticRequest: l => P.regexp(/\s*статистика(\s+за)?/).then(l.date.fallback(new Date())).map(date => new StatisticRequest(date))
 })
+
+export function shiftCurrentMonth(monthsToShift: number) {
+    const d = new Date()
+    d.setMonth(d.getMonth() + monthsToShift)
+    return d
+}
